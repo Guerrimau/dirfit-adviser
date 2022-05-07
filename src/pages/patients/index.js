79237788ff891dd7e2d8dc80115f3d88b547ec6a
Gerardo <gmayboca@gmail.com>
2022-05-06 23:38:45 -0700

    Formulario para agregar paciente

--- src/pages/patients/index.js
+++ src/pages/patients/index.js
@@ -1,18 +1,48 @@
-import { Button } from "@mui/material";
 import React, { useState } from "react";
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, TextField } from "@mui/material";
 import { Sidebar } from "../../components/sidebar";
 
-const paciente = {
-    name: "Gerardo",
-    lastName: "Mayboca",
-    age: 21,
-    dietBefore: true,
-    amountOfPrevDiets: 5,
+const newPatientInitialState = {
+    name: "",
+    lastName: "",
+    age: 0,
+    dietBefore: false,
+    amountOfPrevDiets: 0,
 }
 
 export const PatientsPage = () => {
 
-    const [patients, setPatients] = useState([paciente, paciente]);
+    const [patients, setPatients] = useState([]);
+    const [showAddPatientModal, setShowAddPatientModal] = useState(false);
+    const [newPatient, setNewPatient] = useState(newPatientInitialState);
+
+    const handleOpenAddPatientModal = () => {
+        setShowAddPatientModal(true);
+    };
+
+    const onCloseAddPatientClick = () => {
+        setShowAddPatientModal(false);
+    }
+
+    const onDietBeforeClick = (e) => {
+        setNewPatient(prevState => ({
+            ...prevState,
+            dietBefore: e.target.checked
+        }));
+    }
+
+    const onFormChange = (e) => {
+        setNewPatient(prevState => ({
+            ...prevState,
+            [e.target.id] : e.target.value
+        }));
+    }
+
+    const onAddPatientClick = () => {
+        setPatients(patients.concat([newPatient]));
+        setNewPatient(newPatientInitialState);
+        onCloseAddPatientClick();
+    }
 
     return (
         <div style={{ height: "100vh", display: "grid", gridTemplateColumns: "250px calc(100vw - 250px)" }}>
@@ -24,7 +54,46 @@ export const PatientsPage = () => {
                         <li>{patient.name} {patient.lastName}</li>
                     ))}
                 </ul>
-                <Button>Agregar</Button>
+                <Dialog open={showAddPatientModal} onClose={onCloseAddPatientClick}>
+                    <DialogTitle>Agregar paciente</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            id="name"
+                            label="Nombre"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={onFormChange}/>
+                        <TextField
+                            id="lastName"
+                            label="Apellido"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                            onChange={onFormChange} />
+                        <TextField
+                            id="age"
+                            label="Edad"
+                            type="number"
+                            fullWidth
+                            variant="standard"
+                            onChange={onFormChange} />
+                        <FormControlLabel checked={newPatient?.dietBefore} onChange={onDietBeforeClick} control={<Checkbox /> } label="¿Llevó dieta anteriormente?" />
+                        { newPatient.dietBefore && 
+                            <TextField
+                                id="amountOfPrevDiets"
+                                label="Cantidad de veces"
+                                type="number"
+                                fullWidth
+                                variant="standard"
+                                onChange={onFormChange} /> }
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={onCloseAddPatientClick}>Cancel</Button>
+                        <Button onClick={onAddPatientClick}>Agregar</Button>
+                    </DialogActions>
+                </Dialog>
+                <Button onClick={handleOpenAddPatientModal}>Agregar</Button>
             </main>
         </div>
     )
